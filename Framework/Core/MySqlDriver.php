7c302ee311d28063cb05467f67392c6e00e988c3
BarishYumerov <barish.yumerov@gmail.com>
2015-11-12 17:58:45 +0200

    startover

--- Framework/Core/MySqlDriver.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Framework\Core;
-class MySqlDriver extends Driver {
-
-    public function getDsn() {
-        $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbName;
-        return $dsn;
-    }
-}
