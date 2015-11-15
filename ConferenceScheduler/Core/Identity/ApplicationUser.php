76a144f37beb10c94ed43fbbc4e3d3b37f7e8a1f
BarishYumerov <barish.yumerov@gmail.com>
2015-11-15 15:42:39 +0200

    added default app user model

---
+++ ConferenceScheduler/Core/Identity/ApplicationUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ConferenceScheduler\Core\Identity;
+
+class ApplicationUser {
+    protected $username;
+    protected $email;
+
+    function __construct()
+    {
+    }
+
+    /**
+     * @type varchar(255)
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @type varchar(255)
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
